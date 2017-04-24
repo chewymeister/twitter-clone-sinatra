96cd7f3389f77af89a76911440e19848ba7b4ec0
Jonathan Qiwen Tsang <jtsang@pivotal.io>
2017-04-24 07:56:09 +0100

    Add bootstrap to project
    and first form

--- Gemfile
+++ Gemfile
@@ -3,3 +3,5 @@ source "https://rubygems.org"
 
 gem "sinatra"
 gem "shotgun"
+gem "bootstrap", "~> 4.0.0.alpha6"
+gem "compass"
--- app.rb
+++ app.rb
@@ -1,5 +1,23 @@
 require 'sinatra'
 
+set :user, nil
+
 get '/' do
-  'hello world'
+	erb :index, layout: :layout,
+		locals: {
+		name: 'Jon Tsang',
+		user: user
+	}
+end
+
+post '/' do
+	User = Struct.new(:name, :food)
+	user = User.new(params[:name], params[:food])
+
+	# erb :index, layout: :layout,
+	# 	locals: {
+	# 	name: 'Jon Tsang',
+	# 	user: @user
+	# }
+	redirect '/'
 end
